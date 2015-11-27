ff8bc47397e3c449b8e72d171436e1b9d64b43fc
Kristofer Dubbels <dubbk381@alumni.newschool.edu>
2015-11-27 11:23:36 -0500

    fix parallax thing

--- scripts/main.js
+++ scripts/main.js
@@ -2,4 +2,5 @@ function setHeaderScroll() {
   var height = $(document).outerHeight() - $(window).outerHeight(),
     perc = ($(window).scrollTop()/height*100);
   $('#bar').css({"backgroundPosition": "left "+perc+"%"});
+  console.log(perc);
 }
